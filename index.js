522b29ba39c7469bd5745ca9836a6645d2a20b28
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-30 23:49:27 -0600

    Habilitando la sintaxis del imports y el exports

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require(`express`);
+//Version de comand ----->   const express = require(`express`); 
+import express, { response } from "express"; // version import
 
 const app = express();
 
 
 //Definir puerto
 const port = process.env.PORT || 4000;
+
+app.get(`/`, (req, res) =>{
+    res.send(`Hola mundo`);
+});
+
+app.get(`/Nosotros`, (req, res) =>{
+    res.send(`Nosotros`);
+});
 
 app.listen( port, () =>{
     console.log(`El servidor esta funcionando en el puerto ${port}`);
